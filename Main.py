8b611bf1fe7f2f9c4f2c4f9b44eeccd942f105de
greenDNA <trayvont.york@gmail.com>
2016-12-13 02:57:47 -0500

    Working with Python functions

--- Main.py
+++ Main.py
@@ -12,9 +12,24 @@
  *
  * Layout of foods cooked in Menu class. Further inspection of the food item will on the Menu will go through the Food class
  * and express the Ingredients to make-up said food item.
+ * have a database where information is stored based with personalization details
+ *
  *
  * @author trayvont
  *
  */
 
 """
+
+"""
+The function takes in a list, empty or with items with the intent of processing this list and being able to add, review, or remove elements of the list
+Further, it should call a function or process requests to print to screen or transfer output into a textfile to save data
+Plans to include loading saved data into the program to access data later
+"""
+def listProcessor(input):
+    for element in input:
+        print(element)
+
+print("Welcome to FoodsCooked.py! We look forward to having you use our software and assisting in testing during the next stages of development.\n")
+newList = ["Pizza", "Flour", "Water", "Milk", "Tomato Sauce", "Pepperoni", "Peppers", "Extra Cheese"]
+listProcessor(newList)
